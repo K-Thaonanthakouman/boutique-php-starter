e9550b99ec501811afd36b093c8f084bba8ee713
K-Thaonanthakouman <khonsavant.thao-nantha-kouman@le-campus-numerique.fr>
2026-01-05 14:20:47 +0100

    Jour 1 - Produit.php

--- exercices/jour-01/produit.php
+++ exercices/jour-01/produit.php
@@ -14,19 +14,24 @@
         $stock = 418;
         $onSale = true;
     ?>
-    <br/>
-    <?= var_dump($name); ?>
-    <br/>
-    <?= var_dump($price); ?>
-    <br/>
-    <?= var_dump($stock); ?>
-    <br/>
-    <?= var_dump($onSale); ?>
-    <br/>
+    <p>
+        <?= var_dump($name); ?>
+        <br/>
+        <?= var_dump($price); ?>
+        <br/>
+        <?= var_dump($stock); ?>
+        <br/>
+        <?= var_dump($onSale); ?>
+    </p>
+
     <?= 'Le produit ' . $name . ' coûte ' . $price . ' cacahuètes.' ?>
     <br/>
     <?= 'Il reste encore ' . $stock . ' articles en stock.' ?>
 
+    <p>
+        Quelle différence vois-tu entre var_dump($price) quand $price = "29.99" vs $price = 29.99 ?<br/>
+        - L'un est string, l'autre est float.
+    </p>
 
 </body>
 </html>
