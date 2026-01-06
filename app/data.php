194130c577a175b19c6d6a8e6a8852f4d5e00e57
K-Thaonanthakouman <khonsavant.thao-nantha-kouman@le-campus-numerique.fr>
2026-01-06 15:10:17 +0100

    Produits placés dans le fichier extérieur

---
+++ app/data.php
@@ -0,0 +1,10 @@
+<?php
+    $products = [
+        ["name" => "Smartphone", "prix" => 799.90, "stock" => 18, "catégorie" => "Electronique"],
+        ["name" => "Théière", "prix" => 49,99, "stock" => 46, "catégorie" => "Cuisine"],
+        ["name" => "Ecran plat UHD OLED", "prix" => 1499, "stock" => 4, "catégorie" => "Electronique"],
+        ["name" => "Pommeau de douche", "prix" => 39.99, "stock" => 16, "catégorie" => "Salle de bains"],
+        ["name" => "Sac à dos", "prix" => 69,99, "stock" => 12, "catégorie" => "Accessoires"],
+        ["name" => "Bourriche d'huîtres", "prix" => 19.99, "stock" => 0, "catégorie" => "Nourriture"],
+    ];
+?>
