9a892071b2aae0e0156572247b073b00ecafbd42
K-Thaonanthakouman <khonsavant.thao-nantha-kouman@le-campus-numerique.fr>
2026-01-08 14:29:39 +0100

    Ajout jour 4 labels new et promo

--- app/data.php
+++ app/data.php
@@ -1,10 +1,13 @@
 <?php
     $products = [
-        ["name" => "Smartphone", "prix" => 799.90, "stock" => 18, "catégorie" => "Electronique"],
-        ["name" => "Théière", "prix" => 49,99, "stock" => 46, "catégorie" => "Cuisine"],
-        ["name" => "Ecran plat UHD OLED", "prix" => 1499, "stock" => 4, "catégorie" => "Electronique"],
-        ["name" => "Pommeau de douche", "prix" => 39.99, "stock" => 16, "catégorie" => "Salle de bains"],
-        ["name" => "Sac à dos", "prix" => 69,99, "stock" => 12, "catégorie" => "Accessoires"],
-        ["name" => "Bourriche d'huîtres", "prix" => 19.99, "stock" => 0, "catégorie" => "Nourriture"],
+        ["name" => "Smartphone", "prix" => 799.90, "stock" => 18, "new" => true, "promo" => 5, "catégorie" => "Electronique", "image" => "https://cdn8.futura-sciences.com/a1280/images/actu/images/smartphone_def.jpg"],
+        ["name" => "Théière", "prix" => 49,99, "stock" => 46, "new" => false, "promo" => 0, "catégorie" => "Cuisine", "image" => "https://www.malongo.com/boutique/1162-product_default/theiere-piston-assam-1l-bodum.jpg"],
+        ["name" => "Ecran plat UHD OLED", "prix" => 1499, "stock" => 4, "new" => true, "promo" => 10, "catégorie" => "Electronique", "image" => "https://media.materiel.net/r900/products/MN0006291442_0006291468_0006291477.jpg"],
+        ["name" => "Pommeau de douche", "prix" => 39.99, "stock" => 16, "new" => false, "promo" => 0, "catégorie" => "Salle de bains", "image" => "https://www.manutan.fr/fstrz/r/s/www.manutan.fr/img/S/GRP/ST/AIG5585626.jpg"],
+        ["name" => "Sac à dos", "prix" => 69,99, "stock" => 12, "new" => true, "promo" => 20, "catégorie" => "Accessoires", "image" => "https://www.cdiscount.com/pdt2/8/2/9/1/700x700/leo0733420545829/rw/sac-a-dos-impermeable-sac-de-voyage-sac-a-dos-d-af.jpg"],
+        ["name" => "Bourriche d'huîtres", "prix" => 19.99, "stock" => 0, "new" => false, "promo" => 0, "catégorie" => "Nourriture", "image" => "https://www.laverdura.fr/pub/images/1%20H4%20035.jpg"],
+        ["name" => "Carambar", "prix" => 0.99, "stock" => 4975, "new" => false, "promo" => 0, "catégorie" => "Sucrerie", "image" => "https://media.cdnws.com/_i/168335/783/3742/85/oip-12.jpeg"],
+        ["name" => "Parpaing", "prix" => 5.99, "stock" => 497, "new" => false, "promo" => 0, "catégorie" => "Bâtiment", "image" => "https://i0.wp.com/dh722975772.wpcomstaging.com/wp-content/uploads/2023/08/adobestock_44616368.jpg"],
+
     ];
 ?>
