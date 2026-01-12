afc63c8b2130f5480db9b765e59a1cd1e5289dda
K-Thaonanthakouman <khonsavant.thao-nantha-kouman@le-campus-numerique.fr>
2026-01-12 10:09:03 +0100

    Jour 6 - Produit.php

---
+++ exercices/jour-06/produit.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+    $products = [
+        1 => ["name" => "Ramen", "price" => 6],
+        2 => ["name" => "Maki", "price" => 10],
+        3 => ["name" => "Sushi", "price" => 18],
+        4 => ["name" => "Onigiri", "price" => 8],
+        5 => ["name" => "Mochi", "price" => 12],
+    ];
+
+    if ($products[$_GET["id"]] !== NULL){
+        $id = $_GET["id"];
+        echo 'Voici le produit sélectionné : ' . $products[$id]["name"] . ', ' . $products[$id]["price"] . '€.<br/>';
+    }
+    else if(($_GET["id" === NULL]) || ($products[$id] === NULL)){
+        echo "Aucun produit sélectionné.<br/>";
+    }
+
+
+?>
+    
+</body>
+</html>
