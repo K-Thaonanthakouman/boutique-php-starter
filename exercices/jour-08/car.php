29b7110e6448de65a3173c23b23f56ba1685daef
K-Thaonanthakouman <khonsavant.thao-nantha-kouman@le-campus-numerique.fr>
2026-01-14 15:34:11 +0100

    Jour 8 - Car.php

---
+++ exercices/jour-08/car.php
@@ -0,0 +1,34 @@
+<?php
+class Car
+{
+    public function __construct(string $brand, string $model, int $year)
+    {
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->year = $year;
+    }
+
+    public function getAge(): int
+    {
+        $today = getdate();
+        $age = $today["year"] - ($this->year);
+        return $age;
+    }
+
+    public function display() : void
+    {
+        echo $this->brand . ' ' . $this-> model . ', date d\'il y a ' . $this->getAge() . ' ans.';
+    }
+
+}
+
+// Utilisation
+$car1 = new Car("Ferrari", "458 Italia", 2009);
+$car2 = new Car("Dodge", "Viper VX I", 2013);
+$car3 = new Car("Citroën", "2 CV", 1949);
+
+$car1 -> display();?><br/><?php
+$car2 -> display();?><br/><?php
+$car3 -> display();?><br/><?php
+
+?>
